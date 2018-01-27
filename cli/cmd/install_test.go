a928a15d52d63e7728b95038be36f61913488f25
Andrew Seigner <siggy@buoyant.io>
2018-01-26 16:34:33 -0800

    Simplify and refactor k8s labels and annnotations
    The conduit.io/* k8s labels and annotations we're redundant in some
    cases, and not flexible enough in others.
    
    This change modifies the labels in the following ways:
    `conduit.io/plane: control` => `conduit.io/controller-component: web`
    `conduit.io/controller: conduit` => `conduit.io/controller-ns: conduit`
    `conduit.io/plane: data` => (remove, redundant with `conduit.io/controller-ns`)
    It also centralizes all k8s labels and annotations into
    pkg/k8s/labels.go, and adds tests for the install command.
    
    Part of #201
    
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

---
+++ cli/cmd/install_test.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/sergi/go-diff/diffmatchpatch"
+)
+
+func TestRender(t *testing.T) {
+	t.Run("Should render an install config", func(t *testing.T) {
+		goldenFileBytes, err := ioutil.ReadFile("testdata/install_output.golden")
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		expectedContent := string(goldenFileBytes)
+
+		var buf bytes.Buffer
+
+		config := installConfig{
+			Namespace:                "Namespace",
+			ControllerImage:          "ControllerImage",
+			WebImage:                 "WebImage",
+			PrometheusImage:          "PrometheusImage",
+			ControllerReplicas:       1,
+			WebReplicas:              2,
+			PrometheusReplicas:       3,
+			ImagePullPolicy:          "ImagePullPolicy",
+			UUID:                     "UUID",
+			CliVersion:               "CliVersion",
+			ControllerComponentLabel: "ControllerComponentLabel",
+			CreatedByAnnotation:      "CreatedByAnnotation",
+		}
+
+		err = render(config, &buf)
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		content := buf.String()
+
+		if content != expectedContent {
+			dmp := diffmatchpatch.New()
+			diffs := dmp.DiffMain(content, expectedContent, false)
+			t.Fatalf("Unexpected output: %+v", diffs)
+		}
+	})
+}
